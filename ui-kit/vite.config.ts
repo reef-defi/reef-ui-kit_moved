0514b1135aa13540228a6d84b488bf6a89267616
Samo Šlag <samo.slag@gmail.com>
2022-05-18 13:51:55 +0200

    Add typescript to library

--- ui-kit/vite.config.ts
+++ ui-kit/vite.config.ts
@@ -1,14 +1,16 @@
 const path = require('path')
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/main.tsx'),
       name: '@reef/ui-kit',
+      formats: ['es'],
       fileName: (format) => `main.${format}.js`
     },
     rollupOptions: {
