b67b410c0a9b35105431fa46007d6c2cb8fbe73e
Luka Ločniškar <luka@hudlajf.com>
2022-07-13 11:22:48 +0200

    feat: Add showcase build

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Example from "./example"
 
 const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Example />}/>
-    </Routes>
-  </Router>
+  <Example />
 );
 
 const container = document.getElementById('root')
--- vite.config.ts
+++ vite.config.ts
@@ -7,43 +7,52 @@ import eslint from 'vite-plugin-eslint'
 const path = require('path')
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  if (command === "build") {
+export default defineConfig(({ command, mode }) => {
+  if (command !== "build") {
     return {
-      plugins: [react(), dts()],
-      publicDir: 'dist',
-      build: {
-        lib: {
-          entry: path.resolve(__dirname, 'src/ui-kit/index.tsx'),
-          name: '@reef/ui-kit',
-          formats: ['es'],
-          fileName: (format) => `index.${format}.js`
-        },
-        rollupOptions: {
-          input: path.resolve(__dirname, 'src/ui-kit/index.tsx'),
-          external: [
-            '@fortawesome/fontawesome-svg-core',
-            '@fortawesome/free-regular-svg-icons',
-            '@fortawesome/free-solid-svg-icons',
-            '@fortawesome/react-fontawesome',
-            '@polkadot/react-identicon',
-            '@types/jest',
-            '@types/node',
-            '@types/react',
-            '@types/react-dom',
-            'qrcode.react',
-            'react-syntax-highlighter',
-            'react-transition-group',
-            'react',
-            'react-dom',
-            'react-router-dom',
-            'bignumber.js'
-          ]
-        }
-      }
+      plugins: [react(), checker({ typescript: true }), eslint()]
     }
   }
+
+  if (mode === "showcase") {
+    console.warn("This is showcase")
+    return {
+      base: "",
+      plugins: [react()]
+    }
+  }
+
   return {
-    plugins: [react(), checker({ typescript: true }), eslint()]
+    plugins: [react(), dts()],
+    publicDir: 'dist',
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, 'src/ui-kit/index.tsx'),
+        name: '@reef/ui-kit',
+        formats: ['es'],
+        fileName: (format) => `index.${format}.js`
+      },
+      rollupOptions: {
+        input: path.resolve(__dirname, 'src/ui-kit/index.tsx'),
+        external: [
+          '@fortawesome/fontawesome-svg-core',
+          '@fortawesome/free-regular-svg-icons',
+          '@fortawesome/free-solid-svg-icons',
+          '@fortawesome/react-fontawesome',
+          '@polkadot/react-identicon',
+          '@types/jest',
+          '@types/node',
+          '@types/react',
+          '@types/react-dom',
+          'qrcode.react',
+          'react-syntax-highlighter',
+          'react-transition-group',
+          'react',
+          'react-dom',
+          'react-router-dom',
+          'bignumber.js'
+        ]
+      }
+    }
   }
 })
