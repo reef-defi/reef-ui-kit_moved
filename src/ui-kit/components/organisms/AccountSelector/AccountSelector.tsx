dff51f2e39fa66f2d20b124f15491919ba9f409e
Samo Šlag <samo.slag@gmail.com>
2022-09-08 15:31:39 +0200

    Add network select in Account Selector

--- src/ui-kit/components/organisms/AccountSelector/AccountSelector.tsx
+++ src/ui-kit/components/organisms/AccountSelector/AccountSelector.tsx
@@ -5,6 +5,7 @@ import Icon from "./../../atoms/Icon"
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 import AccountComponent from "./Account"
+import Tabs from "../../atoms/Tabs"
 
 export type Account = {
   name?: string,
@@ -12,12 +13,16 @@ export type Account = {
   evmAddress?: string
 }
 
+export type Network = 'mainnet' | 'testnet'
+
 export interface Props {
   isOpen: boolean,
   accounts?: Account[],
   selectedAccount?: Account | null | undefined,
+  selectedNetwork?: Network,
   onClose?: (...args: any[]) => any,
   onSelect?: (...args: any[]) => any,
+  onNetworkSelect?: (network: Network) => any,
   className?: string
 }
 
@@ -25,8 +30,10 @@ const AccountSelector = ({
   isOpen,
   accounts,
   selectedAccount,
+  selectedNetwork,
   onClose,
   onSelect,
+  onNetworkSelect,
   className
 }: Props): JSX.Element => {
   const wrapper = useRef(null)
@@ -65,6 +72,19 @@ const AccountSelector = ({
           <div className="uik-account-selector__content">
             <div className="uik-account-selector__head">
               <div className="uik-account-selector__title">Select Account</div>
+
+              {
+                !!selectedNetwork && !!onNetworkSelect &&
+                <Tabs
+                  className="uik-account-selector__network"
+                  value={selectedNetwork}
+                  options={[
+                    { value: "mainnet", text: "Mainnet" },
+                    { value: "testnet", text: "Testnet" }
+                  ]}
+                  onChange={onNetworkSelect}
+                />
+              }
 
               <button
                 className="uik-account-selector__close-btn"
